efa822d813cc4c6a518a6efc45ace72648037f1c
Chris Wren <dazedpandas@gmail.com>
2022-01-04 12:12:52 +0000

    Dynamic Paths and Query Paramters

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,7 +1,20 @@
 import React from "react";
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning is fun!" },
+  { id: "q2", author: "Terry", text: "Learning is good for me!" },
+  { id: "q3", author: "Brian", text: "Learning React is amazing!" },
+  { id: "q4", author: "Jerry", text: "Learning React is better than tv!" },
+  {
+    id: "q5",
+    author: "Martin",
+    text: "Learning React is my favourite thing to do!",
+  },
+];
 
 function AllQuotes() {
-  return <h1>All Quotes</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 }
 
 export default AllQuotes;
--- src/pages/SingleQuote.js
+++ src/pages/SingleQuote.js
@@ -1,14 +1,40 @@
 import React, { Fragment } from "react";
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
 
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning is fun!" },
+  { id: "q2", author: "Terry", text: "Learning is good for me!" },
+  { id: "q3", author: "Brian", text: "Learning React is amazing!" },
+  { id: "q4", author: "Jerry", text: "Learning React is better than tv!" },
+  {
+    id: "q5",
+    author: "Martin",
+    text: "Learning React is my favourite thing to do!",
+  },
+];
+
 function SingleQuote() {
+  const match = useRouteMatch();
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No Quote Found</p>;
+  }
   return (
     <Fragment>
-      <h1>Single Quote</h1>
-      <p>{params.quoteId}</p>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={match.path} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
